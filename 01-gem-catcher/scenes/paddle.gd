b08cd60a1802e4033e75d34bc379925826d8a8df
Jason Bird <37068795+JaseBird@users.noreply.github.com>
2025-03-24 13:28:02 +0000

    chore: import gem catcher game

---
+++ 01-gem-catcher/scenes/paddle.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+@export var Speed: float = 200.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var input_ammount: float = Input.get_axis("left", "right")	
+	position.x += delta * Speed * input_ammount
+	
