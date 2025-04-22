4276192744a188893f6173c5baf266bcfbfe2d97
Jason Bird <37068795+JaseBird@users.noreply.github.com>
2025-04-22 12:45:11 +0100

    feat: add image manager

--- 04-memory-madness/Globals/ImageManager.gd
+++ 04-memory-madness/Globals/ImageManager.gd
@@ -1,1 +1,27 @@
 extends Node
+
+const FRAME_IMAGES: Array[Texture2D] = [
+	preload("res://Assets/frames/blue_frame.png"),
+	preload("res://Assets/frames/green_frame.png"),
+	preload("res://Assets/frames/red_frame.png"),
+	preload("res://Assets/frames/yellow_frame.png")
+]
+
+var _image_list: Array[Texture2D]
+
+func _enter_tree() -> void:
+	var ifl: ImageFilesListResource = load("res://Resources/Resources.image_files_list.tres")
+	for file in ifl.file_names:
+		_image_list.append(load(file))	
+
+func get_random_item_image() -> Texture2D:
+	return _image_list.pick_random()
+
+func get_random_frame_image() -> Texture2D:
+	return FRAME_IMAGES.pick_random()
+
+func get_image(index: int) -> Texture2D:
+	return _image_list[index]
+
+func shuffle_images() -> void:
+	_image_list.shuffle()
