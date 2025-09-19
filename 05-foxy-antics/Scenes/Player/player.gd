ebb9792edd6ab3b96dc7c06f2341e5fd3cb5deb6
Jason Bird <37068795+JaseBird@users.noreply.github.com>
2025-09-19 12:50:52 +0100

    feat: add character jump

--- 05-foxy-antics/Scenes/Player/player.gd
+++ 05-foxy-antics/Scenes/Player/player.gd
@@ -5,8 +5,14 @@ class_name Player
 const GRAVITY: float = 690.0
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+const RUN_SPEED = 300.0
 
 
 func _physics_process(delta: float) -> void:
 	velocity.y += GRAVITY * delta
+	jump()
 	move_and_slide()
+
+func jump() -> void:
+	if is_on_floor() and Input.is_action_just_pressed("jump") == true:
+		velocity.y = JUMP_VELOCITY
