7a09d7efebd104dc05863815464a75af83dd87dd
Jason Bird <37068795+JaseBird@users.noreply.github.com>
2025-10-31 10:53:06 +0000

    feat: set Player animation player / tree

--- 05-foxy-antics/Scenes/Player/player.gd
+++ 05-foxy-antics/Scenes/Player/player.gd
@@ -2,14 +2,20 @@ extends CharacterBody2D
 
 class_name Player
 
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
 const GRAVITY: float = 690.0
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-const RUN_SPEED = 300.0
+const JUMP_VELOCITY = -270.0
+const RUN_SPEED = 120.0
 
 
 func _physics_process(delta: float) -> void:
 	velocity.y += GRAVITY * delta
+	velocity.x = RUN_SPEED * Input.get_axis("left", "right")
+	
+	if not is_equal_approx(velocity.x, 0.0):
+		sprite_2d.flip_h = velocity.x < 0
+		
 	jump()
 	move_and_slide()
 
